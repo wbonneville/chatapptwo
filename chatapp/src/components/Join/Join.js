20c5c379e3f672fc7799a553cf6befcadaa0b9f5
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-03 13:00:34 -0500

    Fix typo

--- chatapp/src/components/Join/Join.js
+++ chatapp/src/components/Join/Join.js
@@ -5,7 +5,7 @@ import "./Join.css";
 
 const Join = () => {
   const [name, setName] = useState("");
-  const [room, setRoom] = useSate("");
+  const [room, setRoom] = useState("");
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
