378f59f33547548b8f0ab392414a126253abe75a
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-05 11:19:24 -0500

    final

--- chatapp/src/components/Chat/Chat.js
+++ chatapp/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+  const ENDPOINT = "https://live-react-chat.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
