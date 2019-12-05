7f9598d88b0df91260a9081f8b87ed20123c7ad3
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-05 13:43:44 -0500

    add build

---
+++ chatapp/build/precache-manifest.b1c2a2235e3bfdc9136adabca3855862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2400edebb88b070738cb10b1f2af40e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e04334d053b56d05e607",
+    "url": "/static/css/main.2f0fa4d8.chunk.css"
+  },
+  {
+    "revision": "3c16de9fdd9ae9dc68c3",
+    "url": "/static/js/2.f79a50fb.chunk.js"
+  },
+  {
+    "revision": "e04334d053b56d05e607",
+    "url": "/static/js/main.f7a84a78.chunk.js"
+  },
+  {
+    "revision": "0e52f61209504877fbdc",
+    "url": "/static/js/runtime-main.80c818a0.js"
+  }
+]);
