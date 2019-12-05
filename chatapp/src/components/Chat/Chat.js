32fbc9f9323e383d5f1e45cb94c0326e656219da
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-05 10:09:40 -0500

    fix bug

--- chatapp/src/components/Chat/Chat.js
+++ chatapp/src/components/Chat/Chat.js
@@ -1,81 +1,75 @@
 import React, { useState, useEffect } from "react";
-import "./Chat.css";
-import InfoBar from "../InfoBar/InfoBar";
-import Input from "../Input/Input";
 import queryString from "query-string";
 import io from "socket.io-client";
+
+// import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
+import "./Chat.css";
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-
-  const ENDPOINT = "localhost:5000";
-
-  // useEffect runs when the component renders
-  // retrieve users data
+  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    // pass endpoint to server
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name);
 
-    socket.emit("join", { name, room }, () => {});
+    socket.emit("join", { name, room }, error => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
 
     return () => {
       socket.emit("disconnect");
 
       socket.off();
     };
-    // re-render useEffect only if these two values change
-  }, [ENDPOINT, location.search]);
-
-  useEffect(() => {
-    // listen for messages
-    socket.on("message", message => {
-      // use spread operator
-      // add one message into array of messages
-      // adding all messages to messages array
-      setMessages([...messages, message]);
-    });
-    // only run useEffect when messages array changes
   }, [messages]);
 
-  // create a function for sending messages
-
   const sendMessage = event => {
-    // don't refresh the whole page
     event.preventDefault();
-    // emit listener is in index.js
-    // emit listener listens for the sendMessage event
-    // sends message to server
 
     if (message) {
-      // when you send a message
-      // the message field returns to empty string
       socket.emit("sendMessage", message, () => setMessage(""));
     }
   };
-
-  console.log(message, messages);
 
   return (
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
           sendMessage={sendMessage}
         />
       </div>
+      {/* <TextContainer users={users} /> */}
     </div>
   );
 };
